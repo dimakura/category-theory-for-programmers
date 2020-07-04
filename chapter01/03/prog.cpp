3b95e3a72779d13eff3df398801d86af9a863bd4
Dimitri Kurashvili <dimakura@gmail.com>
2020-07-05 01:19:50 +0400

    Problem 1.3

---
+++ chapter01/03/prog.cpp
@@ -0,0 +1,35 @@
+// $ g++ prog.cpp
+// $ ./a.out
+#include<iostream>
+#include <string>
+using namespace std;
+
+template <class T>
+T id(T x) {
+  return x;
+}
+
+template <class A, class B, class C>
+auto compose(C g(B), B f(A)) {
+  return [=](A a) {
+    return g(f(a));
+  };
+}
+
+int length(string s) { 
+  return s.length();
+}
+
+int main() {
+  auto c1 = length;
+  auto c2 = compose(length, id<string>);
+  auto c3 = compose(id<int>, length);
+
+  string data[] {"1", "12", "123", "1234"};
+
+  for(int i = 0; i < 4; ++i) {
+    cout << c1(data[i]) << " " <<  c2(data[i]) << " " << c3(data[i]) << endl;
+  }
+
+  return 0;
+}
